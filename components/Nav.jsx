36848ce46800d5f097111e598cf67105d8af0165
Kyaw Thu <evanch98@gmail.com>
2023-05-26 02:08:31 +0800

    reformat the code

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -22,11 +22,15 @@ const Nav = () => {
 			</Link>
 			{/* Mobile Navigation */}
 			<div className="sm:flex hidden">
-				{isUserLoggedIn ? <div className="flex gap-3 md:gap-5">
-          <Link href="/create-prompt" className="black_btn">
-            Create Post
-          </Link>
-        </div> : <></>}
+				{isUserLoggedIn ? (
+					<div className="flex gap-3 md:gap-5">
+						<Link href="/create-prompt" className="black_btn">
+							Create Post
+						</Link>
+					</div>
+				) : (
+					<></>
+				)}
 			</div>
 		</nav>
 	);
