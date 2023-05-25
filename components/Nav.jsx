3f331902d3ab994adc8e15d4f202d404823da8d0
Kyaw Thu <evanch98@gmail.com>
2023-05-26 02:12:20 +0800

    add the profile pic with a placeholder image

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -30,6 +30,15 @@ const Nav = () => {
 						<button type="button" onClick={signOut} className="outline_btn">
 							Sign Out
 						</button>
+						<Link href="/profile">
+							<Image
+								src="/assets/images/logo.svg"
+								alt="profile pic"
+								width={37}
+								height={37}
+								className="rounded-full"
+							/>
+						</Link>
 					</div>
 				) : (
 					<></>
