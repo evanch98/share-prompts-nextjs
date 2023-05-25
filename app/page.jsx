d969b578d8e386af7709d9b14acbb81f00fc7a43
Kyaw Thu <evanch98@gmail.com>
2023-05-26 02:08:08 +0800

    minor change

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="md:hidden" />
-        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
         Share Prompts is an open-source AI prompting tool for modern world to discover, create and share creative prompts
