b3503cf83c5dfc53229bc81acca7ea3bdb408104
Kyaw Thu <evanch98@gmail.com>
2023-05-25 21:29:34 +0800

    fix bug and reformat the code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,21 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
 export const metadata = {
-  title: 'Share Prompts',
-  description: 'Discover & Share AI Prompts',
-}
+	title: "Share Prompts",
+	description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = () => {
-  return (
-    <html lang='en'>
-      <body>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
-        <main className='app'>
-          {children}
-        </main>
-      </body>
-    </html>
-  );
+const RootLayout = ({ children }) => {
+	return (
+		<html lang="en">
+			<body>
+				<div className="main">
+					<div className="gradient" />
+				</div>
+				<main className="app">{children}</main>
+			</body>
+		</html>
+	);
 };
 
 export default RootLayout;
