4197733f643f42f19b5cc2f81067251877fd6dbf
Kyaw Thu <evanch98@gmail.com>
2023-05-25 21:22:42 +0800

    add RootLayout

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Share Prompts',
+  description: 'Discover & Share AI Prompts',
+}
+
+const RootLayout = () => {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+        <main className='app'>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
