4ee62dc4a49ddc2970c8db4ecf0f63721a74b3cf
Kyaw Thu <evanch98@gmail.com>
2023-05-26 02:14:51 +0800

    change the isUserLoggedIn to false to start working on the user not logged in UI

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-	const isUserLoggedIn = true;
+	const isUserLoggedIn = false;
 
 	return (
 		<nav className="flex-between w-full mb-16 pt-3">
