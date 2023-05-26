b660e7e948113dfe5f424942a2455f4b1d51fd10
Kyaw Thu <evanch98@gmail.com>
2023-05-26 20:37:55 +0800

    initialize signIn method

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,7 +9,9 @@ const handler = NextAuth({
 		}),
 	],
 	async session({ session }) {},
-	async signIn({ profile }) {},
+	async signIn({ profile }) {
+    try {} catch (error) {}
+  },
 });
 
 export { handler as GET, handler as POST };
