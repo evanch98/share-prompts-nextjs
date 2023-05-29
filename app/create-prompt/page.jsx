24ae90c88260ca8d3888d007e41d15dcafec0ed5
Kyaw Thu <evanch98@gmail.com>
2023-05-30 00:17:33 +0800

    update the createPrompt function

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,25 +4,51 @@ import React, { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Form from "@components/Form";
+import { toast } from "react-hot-toast";
 
 const CreatePrompt = () => {
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
+	const { date: session } = useSession();
+	const router = useRouter();
 
-  const createPrompt = async (e) => {};
+	const [submitting, setSubmitting] = useState(false);
+	const [post, setPost] = useState({
+		prompt: "",
+		tag: "",
+	});
 
-  return (
-    <Form 
-      type="Create"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={createPrompt}
-    />
-  );
+	const createPrompt = async (e) => {
+		e.preventDefault();
+		setSubmitting(true);
+		try {
+			const response = await fetch("/api/prompt/new", {
+				method: "POST",
+				body: JSON.stringify({
+					prompt: post.prompt,
+					userId: session?.user.id,
+					tag: post.tag,
+				}),
+			});
+
+			if (response.ok) {
+        toast.success("Success");
+        router.push("/");
+			}
+		} catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setSubmitting(false);
+    }
+	};
+
+	return (
+		<Form
+			type="Create"
+			post={post}
+			setPost={setPost}
+			submitting={submitting}
+			handleSubmit={createPrompt}
+		/>
+	);
 };
 
 export default CreatePrompt;
