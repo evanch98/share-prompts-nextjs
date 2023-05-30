cb9f25936607b6a1b40676349913df4ec452bb72
Kyaw Thu <evanch98@gmail.com>
2023-05-31 00:47:51 +0800

    add a logic to fetch the posts of the current user

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,19 +6,32 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const ProfilePage = () => {
+	const { data: session } = useSession();
+	const [posts, setPosts] = useState([]);
 
-  const handleEdit = () => {};
-  const handleDelete = () => {};
+	const handleEdit = () => {};
+	const handleDelete = () => {};
 
-  return (
-    <Profile 
-      name="My"
-      desc="Welcome to your personalized profile page"
-      data={[]}
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
-    />
-  );
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch(`/api/users/${session?.user.id}/posts`);
+			const data = await response.json();
+
+			setPosts(data);
+		};
+
+		if (session?.user.id) fetchPosts();
+	}, [setPosts, session]);
+
+	return (
+		<Profile
+			name="My"
+			desc="Welcome to your personalized profile page"
+			data={posts}
+			handleEdit={handleEdit}
+			handleDelete={handleDelete}
+		/>
+	);
 };
 
 export default ProfilePage;
