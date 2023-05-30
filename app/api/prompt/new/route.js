6596a1ee0f7433c4a118dd64640e31b3342a5c40
Kyaw Thu <evanch98@gmail.com>
2023-05-30 20:57:46 +0800

    update the POST function

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,9 +1,23 @@
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+import { toast } from "react-hot-toast";
 
 export const POST = async (req) => {
-  const { userId, prompt, tag } = await req.json();
+	const { userId, prompt, tag } = await req.json();
 
-  try {
-    await connectToDB();
-  } catch (error) {}
-}
+	try {
+		await connectToDB();
+		const newPrompt = new Prompt({
+			creator: userId,
+			prompt,
+			tag,
+		});
+
+		await newPrompt.save();
+
+		return new Response(JSON.stringify(newPrompt), { status: 201 });
+	} catch (error) {
+		toast.error("Failed to create a new prompt.");
+		return new Response("Failed to create a new prompt", { status: 500 });
+	}
+};
