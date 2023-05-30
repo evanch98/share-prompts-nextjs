6739e3d91c780d0994eb60c9c3c1872196c1bf86
Kyaw Thu <evanch98@gmail.com>
2023-05-31 01:10:29 +0800

    change typo

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -6,7 +6,7 @@ import PromptCard from "./PromptCard";
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
 	return (
 		<section className="w-full">
-			<h1 className="text_head text-left">
+			<h1 className="head_text text-left">
 				<span className="blue_gradient">{name} Profile</span>
 			</h1>
 			<p className="desc text-left">{desc}</p>
