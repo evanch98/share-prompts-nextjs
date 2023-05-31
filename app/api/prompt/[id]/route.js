c956511018f0b192c9edcf18e338f382c3c6d751
Kyaw Thu <evanch98@gmail.com>
2023-05-31 11:50:12 +0800

    reformat the code

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -6,9 +6,9 @@ export const GET = async (request, { params }) => {
 	try {
 		await connectToDB();
 		const prompt = await Prompt.findById(params.id).populate("creator");
-    if (!prompt) {
-      return new Response("Prompt not found", { status: 404 });
-    }
+		if (!prompt) {
+			return new Response("Prompt not found", { status: 404 });
+		}
 		return new Response(JSON.stringify(prompt), { status: 200 });
 	} catch (error) {
 		return new Response(
@@ -19,24 +19,24 @@ export const GET = async (request, { params }) => {
 
 // PATCH (update)
 export const PATCH = async (request, { params }) => {
-  const { prompt, tag } = await request.json();
+	const { prompt, tag } = await request.json();
 
-  try {
-    await connectToDB();
+	try {
+		await connectToDB();
 
-    const existingPrompt = await Prompt.findById(params.id);
+		const existingPrompt = await Prompt.findById(params.id);
 
-    if (!existingPrompt) {
-      return new Response("Prompt not found", { status: 404 });
-    }
+		if (!existingPrompt) {
+			return new Response("Prompt not found", { status: 404 });
+		}
 
-    existingPrompt.prompt = prompt;
-    existingPrompt.tag = tag;
-    await existingPrompt.save();
-    return new Response(JSON.stringify(existingPrompt), { status: 200 });
-  } catch (error) {
-    return new Response("Failed to update prompt", { status: 500 });
-  }
+		existingPrompt.prompt = prompt;
+		existingPrompt.tag = tag;
+		await existingPrompt.save();
+		return new Response(JSON.stringify(existingPrompt), { status: 200 });
+	} catch (error) {
+		return new Response("Failed to update prompt", { status: 500 });
+	}
 };
 
 // DELETE (delete)
