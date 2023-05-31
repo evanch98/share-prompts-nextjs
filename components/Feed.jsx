1e2e7d98fd7bb48712946d6bd2047424b5d31a7b
Kyaw Thu <evanch98@gmail.com>
2023-05-31 15:01:20 +0800

    add search feature

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,11 +7,11 @@ const PromptCardList = ({ data, handleTagClick }) => {
 	return (
 		<div className="mt-16 prompt_layout">
 			{data.map((post) => (
-				<PromptCard 
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
+				<PromptCard
+					key={post._id}
+					post={post}
+					handleTagClick={handleTagClick}
+				/>
 			))}
 		</div>
 	);
@@ -19,9 +19,21 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
 	const [searchText, setSearchText] = useState("");
+	const [searchTimeout, setSearchTimeout] = useState(null);
+	const [searchedResults, setSearchedResults] = useState([]);
 	const [posts, setPosts] = useState([]);
 
-	const handleSearchChange = (e) => {};
+	const handleSearchChange = (e) => {
+		clearTimeout(searchTimeout);
+		setSearchText(e.target.value);
+
+		setSearchTimeout(
+			setTimeout(() => {
+				const searchResult = filterPrompts(e.target.value);
+				setSearchedResults(searchResult);
+			}, 500)
+		);
+	};
 
 	useEffect(() => {
 		const fetchPosts = async () => {
@@ -33,6 +45,23 @@ const Feed = () => {
 
 		fetchPosts();
 	}, [setPosts]);
+
+	const filterPrompts = (searchText) => {
+		const regex = new RegExp(searchText, "i");
+		return posts.filter(
+			(item) =>
+				regex.test(item.creator.username) ||
+				regex.test(item.tag) ||
+				regex.test(item.prompt)
+		);
+	};
+
+	const handleTagClick = (tagName) => {
+		setSearchText(tagName);
+
+		const searchResult = filterPrompts(tagName);
+		setSearchedResults(searchResult);
+	};
 
 	return (
 		<section className="feed">
@@ -47,7 +76,14 @@ const Feed = () => {
 				/>
 			</form>
 
-			<PromptCardList data={posts} handleTagClick={() => {}} />
+			{searchText ? (
+				<PromptCardList
+					data={searchedResults}
+					handleTagClick={handleTagClick}
+				/>
+			) : (
+				<PromptCardList data={posts} handleTagClick={handleTagClick} />
+			)}
 		</section>
 	);
 };
