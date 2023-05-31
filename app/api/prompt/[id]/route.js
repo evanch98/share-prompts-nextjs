055c5cd0f936e7c93773ef3d015ca6a4820f9bbe
Kyaw Thu <evanch98@gmail.com>
2023-05-31 11:55:30 +0800

    add the DELETE function

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -40,3 +40,13 @@ export const PATCH = async (request, { params }) => {
 };
 
 // DELETE (delete)
+export const DELETE = async (request, { params }) => {
+	try {
+		await connectToDB();
+
+		await Prompt.findByIdAndRemove(params.id);
+		return new Response("Prompt delete successfully", { status: 200 });
+	} catch (error) {
+		return new Response("Failed to delete prompt", { status: 500 });
+	}
+};
